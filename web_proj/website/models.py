4eb1818156490ad4089ef9390a3b91f3448f5dfe
Jurás Dorka <ijustcantjudo@gmail.com>
2023-12-08 19:20:48 +0100

    Added models, updated templates with the right connection (1:N for user:ratings)

--- web_proj/website/models.py
+++ web_proj/website/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.TextField(max_length=50)
+    age = models.IntegerField()
+    countryOfOrigin = models.TextField(max_length=100)
+
+
+class Rating(models.Model):
+    id = models.AutoField(primary_key=True)
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.TextField(max_length=100)
+    opinion = models.TextField(max_length=1000)
+    rating = models.FloatField()
+
