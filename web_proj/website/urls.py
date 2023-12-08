7f774bcf110e5476f542aaeaaa5e97dc0a719e4b
Jurás Dorka <ijustcantjudo@gmail.com>
2023-12-08 20:33:24 +0100

    Added test views and updated the home page

--- web_proj/website/urls.py
+++ web_proj/website/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('users', views.users, name="users"),
-    path('rating', views.rating, name="rating")
+    path('rating', views.rating, name="rating"),
+    path('addUserTest', views.addUserTest, name="addUserTest"),
+    path('addRatingTest', views.addRatingTest, name="addRatingTest")
 ]
--- web_proj/website/views.py
+++ web_proj/website/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import User, Rating
 
 # Create your views here.
 
@@ -14,3 +16,20 @@ def users(request):
 
 def rating(request):
     return render(request, 'rating.html')
+
+
+def addUserTest(request):
+    User.objects.create(name='Test Name',
+                        age=20,
+                        countryOfOrigin='Canada')
+    return HttpResponseRedirect(reverse("users"))
+
+
+def addRatingTest(request):
+    Rating.objects.create(userId=User(id=1),
+                          title='The Big Test',
+                          opinion="It wasn't the best...",
+                          rating=3.7)
+    return HttpResponseRedirect(reverse("rating"))
+
+
